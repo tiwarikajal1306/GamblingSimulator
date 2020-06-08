f22264c9121d577ad98818d5f8132ca412efb353
kajal <tiwarikajal1306@gmail.com>
2020-06-09 01:03:42 +0530

    bug in calculation of total amount resolved

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,34 +1,35 @@
-#!/bin/bash -x
 STACK=100
 WIN=1
 LOSS=0
+totalWin=0
+totalLoss=0
 numOfDays=20
-totalWinOrloss=100
+totalWinOrloss=0
 stakePercent=$(( 50*$STACK/100 ))
 max_win=$(( $stakePercent+$STACK ))
 max_loss=$(( $STACK-$stakePercent ))
 for (( day=0; day<$numOfDays; day++ ))
 do
-	while [ $STACK -lt $max_win ] && [ $STACK -gt $max_loss ]
+	STACK=100
+	while [[ $STACK -lt $max_win && $STACK -gt $max_loss ]]
 	do
-		randomCheck=$(( RANDOM%2 ))
+		randomCheck=$((RANDOM%2))
 		if [ $randomCheck -eq $WIN ]
         	then
-                	echo "Bet is win"
 			STACK=$(($STACK+1))
         	else
-			echo "Bet is loss"
 			STACK=$(($STACK-1))
         	fi
 	done
-
 	if [ $STACK -eq $max_loss ]
 	then
+		((totalLoss++))
 		totalWinOrloss=$(( $totalWinOrloss-50 ))
 	else
+		((totalWin++))
         	totalWinOrloss=$(( $totalWinOrloss+50 ))
 	fi
         	echo "Resign for the day"
         done
 
-echo $totalWinOrloss
+echo $totalWinOrloss $totalLoss $totalWin
