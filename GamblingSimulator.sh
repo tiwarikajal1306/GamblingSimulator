6c930dcbc0c16922d6b79ea751e83610746d5222
kajal <tiwarikajal1306@gmail.com>
2020-06-08 23:26:03 +0530

    total amount won or loss after 20 days

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,23 +2,33 @@
 STACK=100
 WIN=1
 LOSS=0
-
+numOfDays=20
+totalWinOrloss=100
 stakePercent=$(( 50*$STACK/100 ))
 max_win=$(( $stakePercent+$STACK ))
 max_loss=$(( $STACK-$stakePercent ))
-
-while [ $STACK -lt $max_win ] && [ $STACK -gt $max_loss ]
+for (( day=0; day<$numOfDays; day++ ))
 do
-	randomCheck=$(( RANDOM%2 ))
-	if [ $randomCheck -eq $WIN ]
-        then
-                echo "Bet is win"
-		STACK=$(($STACK+1))
-        else
-		echo "Bet is loss"
-		STACK=$(($STACK-1))
-        fi
-done
+	while [ $STACK -lt $max_win ] && [ $STACK -gt $max_loss ]
+	do
+		randomCheck=$(( RANDOM%2 ))
+		if [ $randomCheck -eq $WIN ]
+        	then
+                	echo "Bet is win"
+			STACK=$(($STACK+1))
+        	else
+			echo "Bet is loss"
+			STACK=$(($STACK-1))
+        	fi
+	done
 
-echo "STACK containing $STACK"
-echo "Resign for the day"
+	if [ $STACK -eq $max_loss ]
+	then
+		totalWinOrloss=$(( $totalWinOrloss-50 ))
+	else
+        	totalWinOrloss=$(( $totalWinOrloss+50 ))
+	fi
+        	echo "Resign for the day"
+        done
+
+echo $totalWinOrloss
