c02c543358049c7b38c5a66a30e75d47c9fa7e03
kajal <tiwarikajal1306@gmail.com>
2020-06-08 20:24:02 +0530

    resign the game after reaching max_win and Max_loss condition

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,7 +2,14 @@
 STACK=100
 WIN=1
 LOSS=0
-randomCheck=$(( RANDOM%2 ))
+
+stakePercent=$(( 50*$STACK/100 ))
+max_win=$(( $stakePercent+$STACK ))
+max_loss=$(( $STACK-$stakePercent ))
+
+while [ $STACK -lt $max_win ] && [ $STACK -gt $max_loss ]
+do
+	randomCheck=$(( RANDOM%2 ))
 	if [ $randomCheck -eq $WIN ]
         then
                 echo "Bet is win"
@@ -11,4 +18,7 @@ randomCheck=$(( RANDOM%2 ))
 		echo "Bet is loss"
 		STACK=$(($STACK-1))
         fi
+done
+
 echo "STACK containing $STACK"
+echo "Resign for the day"
